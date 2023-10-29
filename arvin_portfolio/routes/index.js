599fdefc3fc1dba80588fc2e6cdb9e5dc7f0732f
Arvin Ziba <arvin.ziba@ontariotechu.net>
2023-10-29 15:16:13 -0400

    1. Configured the express routes. 2. Written the main EJS template. 3. Tested server to client connectivity.

--- arvin_portfolio/routes/index.js
+++ arvin_portfolio/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {     // external code 
+  res.render('layout', {
+      pageTitle: 'Home',
+      body: 'index',
+  });
 });
+
+
+router.get('/', (req, res) => {     // external code 
+  res.render('layout', {
+      pageTitle: 'Projects',
+      body: 'projects',
+  });
+});
+
+
+router.get('/', (req, res) => {     // external code 
+  res.render('layout', {
+      pageTitle: 'Contact',
+      body: 'contact',
+  });
+});
+
+
+router.get('/', (req, res) => {     // external code 
+  res.render('layout', {
+      pageTitle: 'About',
+      body: 'about',
+  });
+});
+
+
 
 module.exports = router;
